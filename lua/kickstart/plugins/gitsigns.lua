bebb6bf360619eb842b2eb087d916a35c338af58
Danila Makulov <danila.makulov@pipedrive.com>
2026-02-02 19:07:35 +0100

    add git-signs

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -24,7 +24,23 @@ return {
           end
         end, { desc = 'Jump to next git [c]hange' })
 
+        map('n', '<C-j>', function()
+          if vim.wo.diff then
+            vim.cmd.normal { ']c', bang = true }
+          else
+            gitsigns.nav_hunk 'next'
+          end
+        end, { desc = 'Jump to next git [c]hange' })
+
         map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { '[c', bang = true }
+          else
+            gitsigns.nav_hunk 'prev'
+          end
+        end, { desc = 'Jump to previous git [c]hange' })
+
+        map('n', '<C-k>', function()
           if vim.wo.diff then
             vim.cmd.normal { '[c', bang = true }
           else
