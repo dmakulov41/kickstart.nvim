37b0f4e766b79160e5196dcb6d63a328ab7ae0a5
Danila Makulov <danila.makulov@pipedrive.com>
2026-02-05 22:35:06 +0100

    nvim: render icons in telescope

--- init.lua
+++ init.lua
@@ -329,9 +329,34 @@ require('lazy').setup({
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      { 'nvim-tree/nvim-web-devicons', enabled = true },
     },
     config = function()
+      local function normalize_path(path) return path:gsub('\\', '/') end
+
+      local function normalize_cwd() return normalize_path(vim.loop.cwd()) .. '/' end
+
+      local function is_subdirectory(cwd, path) return string.lower(path:sub(1, #cwd)) == string.lower(cwd) end
+
+      local function split_filepath(path)
+        local normalized_path = normalize_path(path)
+        local normalized_cwd = normalize_cwd()
+        local filename = normalized_path:match '[^/]+$'
+
+        if is_subdirectory(normalized_cwd, normalized_path) then
+          local stripped_path = normalized_path:sub(#normalized_cwd + 1, -(#filename + 1))
+          return stripped_path, filename
+        else
+          local stripped_path = normalized_path:sub(1, -(#filename + 1))
+          return stripped_path, filename
+        end
+      end
+
+      local function path_display(_, path)
+        local stripped_path, filename = split_filepath(path)
+        if filename == stripped_path or stripped_path == '' then return filename end
+        return string.format('%s ~ %s', filename, stripped_path)
+      end
       -- Telescope is a fuzzy finder that comes with a lot of different things that
       -- it can fuzzy find! It's more than just a "file finder", it can search
       -- many different aspects of Neovim, your workspace, LSP, and more!
