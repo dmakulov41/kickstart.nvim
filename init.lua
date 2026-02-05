99f96c7683899ae80b8c675b6f3316a0dd8cb394
Danila Makulov <danila.makulov@pipedrive.com>
2026-02-05 22:44:22 +0100

    nvim: add auto-save

--- init.lua
+++ init.lua
@@ -862,6 +862,7 @@ require('lazy').setup({
 
   require 'kickstart.plugins.blame',
   require 'kickstart.plugins.copilot',
+  { '907th/vim-auto-save', init = function() vim.g.auto_save = 1 end },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
