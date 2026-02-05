e39cd30cce5b17b7af75f67b4b01727e1401b019
Danila Makulov <danila.makulov@pipedrive.com>
2026-02-05 22:31:28 +0100

    nvim: disable git-blame

--- lua/kickstart/plugins/blame.lua
+++ lua/kickstart/plugins/blame.lua
@@ -9,8 +9,8 @@ return {
   opts = {
     -- your configuration comes here
     -- for example
-    enabled = true, -- if you want to enable the plugin
-    message_template = ' <summary> • <date> • <author> • <<sha>>', -- template for the blame message, check the Message template section for more options
+    enabled = false, -- if you want to enable the plugin
+    message_template = '<author>, <date> <summary> • <<sha>>', -- template for the blame message, check the Message template section for more options
     date_format = '%d-%m-%Y %H:%M', -- template for the date, check Date format section for more options
     virtual_text_column = 80, -- virtual text start column, check Start virtual text at column section for more options
   },
