2617911db304bdb9796cada035c761664500beed
Danila Makulov <danila.makulov@pipedrive.com>
2026-02-05 22:44:53 +0100

    nvim: add sm

--- init.lua
+++ init.lua
@@ -863,6 +863,7 @@ require('lazy').setup({
   require 'kickstart.plugins.blame',
   require 'kickstart.plugins.copilot',
   { '907th/vim-auto-save', init = function() vim.g.auto_save = 1 end },
+  { 'psliwka/vim-smoothie' },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
