36ad86e73e226a02e22b2cf7dcd22457da9192cd
Danila Makulov <danila.makulov@pipedrive.com>
2026-02-05 22:46:01 +0100

    nvim: add mini diagnostics plugin

--- init.lua
+++ init.lua
@@ -869,6 +869,17 @@ require('lazy').setup({
     'nvim-telescope/telescope-frecency.nvim',
     config = function() require('telescope').load_extension 'frecency' end,
   },
+  {
+    'rachartier/tiny-inline-diagnostic.nvim',
+    event = 'VeryLazy',
+    priority = 1000,
+    config = function()
+      require('tiny-inline-diagnostic').setup {
+        preset = 'amongus',
+      }
+      vim.diagnostic.config { virtual_text = false } -- Disable Neovim's default virtual text diagnostics
+    end,
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
